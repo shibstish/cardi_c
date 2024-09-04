c9fb7bdf2698675e1f5c0590c17ad6cb00d16e93
shibstish <78878993+shibstish@users.noreply.github.com>
2024-09-04 14:30:22 -0700

    plink_vcf2bed.sh

--- plink_vcf2bed.sh
+++ plink_vcf2bed.sh
@@ -10,4 +10,4 @@ module load plink/1.9
 
 plink --vcf [path-to-vcf] --allow-extra-chr --indep 50 5 2 #this creates files "plink.prune.in" and "plink.prune.out"
 
-plink --vcf /data/busch/stisinai/Ccordifolia/dna/VCF/Ccordifolia.f.vcf --allow-extra-chr --maf 0.05 --extract plink.prune.in --geno 0 --make-bed --out /[path-to-outfile]
+plink --vcf [path_to_VCF_file]/Ccordifolia.f.vcf --allow-extra-chr --maf 0.05 --extract plink.prune.in --geno 0 --make-bed --out /[path-to-outfile]
